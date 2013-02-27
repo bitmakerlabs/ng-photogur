eddf789e3c81db7ef3d5490f9ce9c4f691900281
Monica Olinescu <monica.olinescu@gmail.com>
2013-02-27 18:53:44 -0500

    Installing Angular through the Rails asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+
+// VENDOR LIBRARIES
 //= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require angular-1.0.5.min
